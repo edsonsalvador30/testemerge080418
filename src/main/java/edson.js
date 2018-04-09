7580c27d642b372d49030ac12ea3dea499824090
edson.fonseca@techne.com.br <edson.fonseca@techne.com.br>
2018-04-09 00:53:41 +0000

    1

--- src/main/java/edson.js
+++ src/main/java/edson.js
@@ -1,2 +1,23 @@
 var myHeading = document.querySelector('h1');
-myHeading.innerHTML = 'Hello world!';
+myHeading.innerHTML = 'Hello Junior!';
+
+myHeading.innerHTML = 'Hello Junior!';
+myHeading.innerHTML = 'Hello Junior!';
+myHeading.innerHTML = 'Hello Junior!';
+myHeading.innerHTML = 'Hello Junior!';
+myHeading.innerHTML = 'Hello Junior!';
+myHeading.innerHTML = 'Hello Junior!';
+
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello Junior!';
+
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello Junior!';
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello Junior!';
+
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello Junior!';
+
+var myHeading = document.querySelector('h1');
+myHeading.innerHTML = 'Hello Junior!';
